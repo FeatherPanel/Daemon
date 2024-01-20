624982f06896e03af6686f3ad7432550cd4d30df
Natoune <contact@natoune.fr>
2024-01-20 18:25:35 +0100

    :sparkles: Send node disk info to admins

--- src/api/system-info.ts
+++ src/api/system-info.ts
@@ -1,7 +1,9 @@
+import checkDiskSpace from "check-disk-space";
 import { Request, Response } from "express";
 import os from "os";
 
 import { VERSION } from "../constants";
+import { docker } from "../utils/docker";
 import { validToken } from "../utils/token";
 
 module.exports = {
@@ -14,25 +16,35 @@ module.exports = {
 				error: "UNAUTHORIZED",
 			});
 
-		return res.status(200).json({
-			status: "success",
-			message: "System info",
-			data: {
-				os: {
-					arch: os.arch(),
-					cpus: os.cpus(),
-					freemem: os.freemem(),
-					hostname: os.hostname(),
-					networkInterfaces: os.networkInterfaces(),
-					platform: os.platform(),
-					totalmem: os.totalmem(),
-					type: os.type(),
-					uptime: os.uptime(),
-				},
-				daemon: {
-					version: VERSION,
-				},
-			},
+		checkDiskSpace(
+			os.platform() === "win32"
+				? "\\\\wsl$\\docker-desktop-data\\data\\docker\\volumes"
+				: "/var/lib/docker/volumes"
+		).then((diskSpace: any) => {
+			docker.info((err: any, dockerInfo: any) => {
+				return res.status(200).json({
+					status: "success",
+					message: "System info",
+					data: {
+						os: {
+							arch: os.arch(),
+							cpus: os.cpus(),
+							freemem: os.freemem(),
+							hostname: os.hostname(),
+							networkInterfaces: os.networkInterfaces(),
+							platform: os.platform(),
+							totalmem: os.totalmem(),
+							type: os.type(),
+							uptime: os.uptime(),
+						},
+						daemon: {
+							version: VERSION,
+						},
+						docker: dockerInfo,
+						disk: diskSpace,
+					},
+				});
+			});
 		});
 	},
 };
